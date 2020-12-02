8b731852d50731ddb5353d54752a53356a94e8cc
sherrywilly <sherrywilson521@gmail.com>
2020-12-02 11:29:07 +0530

    routes aand some basic stuffs

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const app = express()
 
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello world</h1>");
-});
+app.use(express.static("view"))
 
-app.get("*", (req, res) => {
-  res.status(400).send("404");
-});
 
-const PORT = process.env.PORT || 3001;
+app.get('/',(req, res)=>{
+    res.json("<h1>hello world</h1>")
+})
 
-app.listen(PORT, () => {
-  console.log("server is running");
-});
+app.get('/contact',(req, res)=>{
+    res.send("<h1>contact page</h1>")
+})
+app.get('/about',(req, res)=>{
+    res.sendFile(path.join(__dirname,"views","about.html"))
+})
+
+app.get('*',(req, res)=>{
+    res.status(404).send("404")
+})
+
+const PORT = process.env.PORT||3001;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port : ${PORT}`)
+})
